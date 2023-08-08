69332cd77da12a636666ea645b039035604507e4
unknown <crawford@email.com>
2023-08-08 10:09:55 +0100

    init

---
+++ selfies.js
@@ -0,0 +1,309 @@
+
+const selfies = [
+	{
+		OriginalUri : "1e550357-0c97-4455-9120-709dab113213.jpg"
+	},
+	{
+		OriginalUri : "dde1c4a7-4c11-4213-84ee-5e6cdffef00e.jpg"
+	},
+	{
+		OriginalUri : "7ad0d17a-10e1-47ea-8816-9322f6849334.jpg"
+	},
+	{
+		OriginalUri : "668ea03d-5022-4a2f-a555-247c96685968.jpg"
+	},
+	{
+		OriginalUri : "3e13dec2-6fc9-4a9a-9fb2-acf3247bd2a9.jpg"
+	},
+	{
+		OriginalUri : "19f2220b-4286-4432-ab1a-3f9481525803.jpg"
+	},
+	{
+		OriginalUri : "5b0fd3b9-92d9-4c0e-a064-3f6f7ffed6d5.jpg"
+	},
+	{
+		OriginalUri : "918c0fb7-7baf-43d2-863a-e3e72ead3b77.jpg"
+	},
+	{
+		OriginalUri : "423c997c-bee8-428f-9785-8122e12d8d2e.jpg"
+	},
+	{
+		OriginalUri : "aca17dc9-b157-4ec3-b9c7-1510398fb1dc.jpg"
+	},
+	{
+		OriginalUri : "6e13c681-3f8e-4577-ba41-67f1d9a35bc3.jpg"
+	},
+	{
+		OriginalUri : "c787e7aa-d6e0-4b13-88bb-b6c66dd99314.jpg"
+	},
+	{
+		OriginalUri : "5aa1ddc0-b627-4d16-b2ab-f41e96d0b013.jpg"
+	},
+	{
+		OriginalUri : "130876cd-2124-48ba-9090-8e5ed475dc3e.jpg"
+	},
+	{
+		OriginalUri : "22bba29a-7c79-4dd0-8705-975ef890f479.jpg"
+	},
+	{
+		OriginalUri : "4a0622c7-9771-4fdf-9824-af757cca1bc2.jpg"
+	},
+	{
+		OriginalUri : "3388781f-d991-4b1e-b6ee-b3232703407e.jpg"
+	},
+	{
+		OriginalUri : "858c9dad-ed1c-430a-bbb6-2c77d15b4f7c.jpg"
+	},
+	{
+		OriginalUri : "d897adea-a54d-45c6-86e6-59dd84d039f8.jpg"
+	},
+	{
+		OriginalUri : "9b83e263-0d9b-440d-b4e3-68699352dcc4.jpg"
+	},
+	{
+		OriginalUri : "9f9571c1-5e47-47b3-b19b-6973e99e683d.jpg"
+	},
+	{
+		OriginalUri : "59c17761-e84b-4735-9307-67335484582a.jpg"
+	},
+	{
+		OriginalUri : "07d71001-db95-4692-8317-3fcfea1c0fff.jpg"
+	},
+	{
+		OriginalUri : "b3d22825-fa86-43f3-be9c-c994a53f1642.jpg"
+	},
+	{
+		OriginalUri : "4248bf02-006d-47f5-af0d-60458c224ab0.jpg"
+	},
+	{
+		OriginalUri : "ecd03002-6a48-4227-bea5-4266cc1491c5.jpg"
+	},
+	{
+		OriginalUri : "803cbb9c-4377-4d37-a514-a8ec6cc7b42e.jpg"
+	},
+	{
+		OriginalUri : "6ded45db-fb75-4f73-89af-a021d86deb00.jpg"
+	},
+	{
+		OriginalUri : "d3ce5c9a-2407-42a1-8ad2-6e44a1e0161d.jpg"
+	},
+	{
+		OriginalUri : "d104a65b-2b23-4073-b6bf-5cd43403a294.jpg"
+	},
+	{
+		OriginalUri : "68d09e24-3b4d-4854-acac-503c44b36bf6.jpg"
+	},
+	{
+		OriginalUri : "7e08a954-baf6-4382-96c3-5d37bcb402ee.jpg"
+	},
+	{
+		OriginalUri : "5a9d1c58-ec8e-4674-8c5a-956d799a4456.jpg"
+	},
+	{
+		OriginalUri : "9fabca47-45b3-4cbb-97cc-21621f8abb2d.jpg"
+	},
+	{
+		OriginalUri : "cd4ef261-4c3c-488c-a4c7-c27ab8b9c271.jpg"
+	},
+	{
+		OriginalUri : "984bf4d5-b0d4-4ad5-8c73-6bd6a4a329b4.jpg"
+	},
+	{
+		OriginalUri : "ca7304c6-b8cd-444b-9f65-2bcac97b0ae3.jpg"
+	},
+	{
+		OriginalUri : "69f9172a-13b5-46b6-a458-768ddee24bb2.jpg"
+	},
+	{
+		OriginalUri : "7112e7cc-3c8c-4ec1-a752-3dbe54f55c1e.jpg"
+	},
+	{
+		OriginalUri : "703a6498-c81c-4c71-b3a2-57e8d0b03a54.jpg"
+	},
+	{
+		OriginalUri : "1580800b-0fbf-4096-ab69-1377d8ffe65d.jpg"
+	},
+	{
+		OriginalUri : "e771eb9d-90a2-4a9d-9fa5-70ed7e23d412.jpg"
+	},
+	{
+		OriginalUri : "dbf20222-2e1d-4715-b7d8-07725eef520d.jpg"
+	},
+	{
+		OriginalUri : "2d7e60a2-5d18-4d51-9929-a787b55f3160.jpg"
+	},
+	{
+		OriginalUri : "683de0fe-2fc2-408a-b3a6-072325f6f155.jpg"
+	},
+	{
+		OriginalUri : "aa7bb06b-a91e-41f3-b6fd-708d1589cf7f.jpg"
+	},
+	{
+		OriginalUri : "39a2c318-fcdb-4923-b068-d9e4b5012b50.jpg"
+	},
+	{
+		OriginalUri : "2ac6dd24-8323-4e89-aa90-f47a7078c394.jpg"
+	},
+	{
+		OriginalUri : "49a0a15d-6acc-458e-b8dc-9a603024a3c2.jpg"
+	},
+	{
+		OriginalUri : "90b3c739-8424-4990-b13b-d7cc7aa9bfdb.jpg"
+	},
+	{
+		OriginalUri : "468e1541-b853-48a2-a91a-ba67cd439052.jpg"
+	},
+	{
+		OriginalUri : "b2cbc47f-50c9-4a7b-993b-d773aff78d45.jpg"
+	},
+	{
+		OriginalUri : "d1e19433-aa86-4bc5-b8ee-49d53f62640c.jpg"
+	},
+	{
+		OriginalUri : "106a0046-2baa-481d-9a76-a3f52ae5f174.jpg"
+	},
+	{
+		OriginalUri : "fbe261be-e8ba-431c-b0f9-4a66ac208955.jpg"
+	},
+	{
+		OriginalUri : "d04060f3-ca5b-4504-9426-90820af40ad1.jpg"
+	},
+	{
+		OriginalUri : "f47ef7ce-5fdc-4743-b6e0-667009ed7674.jpg"
+	},
+	{
+		OriginalUri : "683014c8-fa8d-4be1-b4e5-731cd1e2d350.jpg"
+	},
+	{
+		OriginalUri : "b3c86df3-46b1-49b2-82ef-e39f55bdcf98.jpg"
+	},
+	{
+		OriginalUri : "3df7e12f-b397-4eb7-bffc-a2df8d416dd8.jpg"
+	},
+	{
+		OriginalUri : "19ad5a09-b2fc-4b2f-a0c4-5f0187294488.jpg"
+	},
+	{
+		OriginalUri : "7c70c211-927c-4e69-8dce-f8f0c8e313c8.jpg"
+	},
+	{
+		OriginalUri : "673517d1-2501-45de-9418-fade50bdc012.jpg"
+	},
+	{
+		OriginalUri : "cb9f9c00-66ad-437b-83ac-e8264c1bdd01.jpg"
+	},
+	{
+		OriginalUri : "9f0e9765-86ca-4ea2-b4d5-9c85ed510639.jpg"
+	},
+	{
+		OriginalUri : "864920b5-0d81-4435-8b65-1be92dbd4297.jpg"
+	},
+	{
+		OriginalUri : "fc9eaf9b-ce95-4568-bb47-46e2a90d46bc.jpg"
+	},
+	{
+		OriginalUri : "ef894840-51b4-4e4f-9243-8c564ac93747.jpg"
+	},
+	{
+		OriginalUri : "0d9bceb9-94ad-4099-865a-ef8a98b70b86.jpg"
+	},
+	{
+		OriginalUri : "b74e31a1-60c3-479e-8039-02aa1c45b1c5.jpg"
+	},
+	{
+		OriginalUri : "d3b60c29-19d8-4fd3-952b-2ddb14bfb68a.jpg"
+	},
+	{
+		OriginalUri : "ea5933c2-254c-4d55-bf55-292df032b6d9.jpg"
+	},
+	{
+		OriginalUri : "aea0694d-d8da-46cf-a3e6-be158d0634f4.jpg"
+	},
+	{
+		OriginalUri : "a6de3f2b-9df5-4ba6-9bbb-070d491f3e8c.jpg"
+	},
+	{
+		OriginalUri : "da9ef97a-e3e4-4dab-9b34-d56ce2340895.jpg"
+	},
+	{
+		OriginalUri : "8b63fc45-6238-4788-a860-8257529eecc0.jpg"
+	},
+	{
+		OriginalUri : "36d72629-41a7-4212-a944-f2937b0f0dcf.jpg"
+	},
+	{
+		OriginalUri : "fba0a47f-9a19-4096-80ed-0bd3427ab6aa.jpg"
+	},
+	{
+		OriginalUri : "4e0a0963-8b1d-45dc-9099-0d8e5b12397f.jpg"
+	},
+	{
+		OriginalUri : "aa6b7fd5-7fda-4b45-927f-12a6b4bb52cf.jpg"
+	},
+	{
+		OriginalUri : "ad39b371-9893-4c00-a92d-d7d4f93683cc.jpg"
+	},
+	{
+		OriginalUri : "2d06f3cb-0fb3-46cd-aa9b-456008f2f298.jpg"
+	},
+	{
+		OriginalUri : "546e30f5-c7c5-4ea1-9d9b-ea15d8657210.jpg"
+	},
+	{
+		OriginalUri : "360124ba-5b77-423f-9137-78b01a5ca0f3.jpg"
+	},
+	{
+		OriginalUri : "dae05234-c581-4ade-93c2-4063906bc356.jpg"
+	},
+	{
+		OriginalUri : "cf8b7a84-0817-4b16-9a3d-bbdce26c73b6.jpg"
+	},
+	{
+		OriginalUri : "cc356a91-b8f1-47c3-a9ba-459c1f20437b.jpg"
+	},
+	{
+		OriginalUri : "3473678c-cf64-4fa7-bb9a-c4d48c9da15d.jpg"
+	},
+	{
+		OriginalUri : "7cd64d9f-c03a-4cff-a47b-1c9b68646705.jpg"
+	},
+	{
+		OriginalUri : "ff1dbcc5-bf6e-43e3-88d3-a6afb2bfe032.jpg"
+	},
+	{
+		OriginalUri : "324265e1-3576-4807-b5b4-37192ca066a2.jpg"
+	},
+	{
+		OriginalUri : "ef3d56ed-ba0b-4e66-852a-c5b60629266c.jpg"
+	},
+	{
+		OriginalUri : "d987d9e1-a29b-43ea-b68d-6f8f2ffa8778.jpg"
+	},
+	{
+		OriginalUri : "5db7f538-72dd-4f49-bba9-dc1966ed266e.jpg"
+	},
+	{
+		OriginalUri : "41db1f48-6607-405a-bc96-ae3755888092.jpg"
+	},
+	{
+		OriginalUri : "b0f7dbc5-2bde-4253-b6ca-2996f3d6b57b.jpg"
+	},
+	{
+		OriginalUri : "c7ea76ea-ed8c-4179-8358-5999caa42932.jpg"
+	},
+	{
+		OriginalUri : "997d5867-5fa9-4178-bb7d-9f9d5c857614.jpg"
+	},
+	{
+		OriginalUri : "e9f164a8-dbb9-4e89-905c-eff57a82b285.jpg"
+	},
+	{
+		OriginalUri : "4256f5af-b674-4546-8ff7-3e63bf86a95a.jpg"
+	},
+	{
+		OriginalUri : "02943a70-bcc8-43ac-ac48-e11f570e6ed5.jpg"
+	},
+	{
+		OriginalUri : "47a6be76-485f-49ac-82e8-278ac3fe1a44.jpg"
+	}
+];
